bd217150b02ee2fa13f90d1aa24e82e532c2e3ec
Connor Claflin <cclaflin33@gmail.com>
2015-02-22 23:52:41 -0500

    Beginning of setting up add page page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'WikiStack' });
 });
 
+router.get('/', function(req, res, next) {
+  res.render('addPage', { title: 'AddPage' });
+});
+
 module.exports = router;
